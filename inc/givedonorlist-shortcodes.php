2ec327f0c3ff62c868417f592fb511aa2bf7741f
Florian TIAR <contact@tiar-florian.fr>
2017-11-28 17:45:20 +0100

    Fix wordpress coding standards

--- inc/givedonorlist-shortcodes.php
+++ inc/givedonorlist-shortcodes.php
@@ -1,17 +1,19 @@
 <?php
 /**
-  * Version: 1.0
-  * Author: Jason Tucker
-  * Author URI: http://www.jasontucker.us/
-  * License: GPLv2
-  *
-  **/
+ * Version: 1.0
+ * Author: Jason Tucker
+ * Author URI: http://www.jasontucker.us/
+ * License: GPLv2
+ *
+ **/
 
 add_shortcode( 'give_donor_list', 'recent_givedonorlist' );
 
 /**
  * Output recent GIVE donors in a list
- * @param array $atts 
+ *
+ * @param array $atts
+ *
  * @return string of names comma seperated
  */
 function recent_givedonorlist( $atts ) {
@@ -21,26 +23,27 @@ function recent_givedonorlist( $atts ) {
 	}
 
 	$args = shortcode_atts(
-        array(
-            'number'   =>   '100',
-            'id'       =>   '1'
-        ), $atts
-    );
+		array(
+			'number' => '100',
+			'id'     => '1'
+		), $atts
+	);
 
-    $number = (int) $args['number'];
-    $id = (int) $args['id'];
+	$number = (int) $args['number'];
+	$id     = (int) $args['id'];
 
-    $output = '';
+	$output = '';
 
-    // First check that Give exist
-    $donors = Give()->customers->get_customers( $number, $id );
-    if ( is_array( $donors ) && ! empty( $donors ) ) {
-	    shuffle( $donors );
-	    $output = '<ul class="give-donors-list">';
-	    foreach ( $donors as $donor ) {
-		    $output .= '<li>' . $donor->name . "</li>";
-	    }
-	    $output .= '</ul>';
-    }
-    return $output;
+	// First check that Give exist
+	$donors = Give()->customers->get_customers( $number, $id );
+	if ( is_array( $donors ) && ! empty( $donors ) ) {
+		shuffle( $donors );
+		$output = '<ul class="give-donors-list">';
+		foreach ( $donors as $donor ) {
+			$output .= '<li>' . $donor->name . "</li>";
+		}
+		$output .= '</ul>';
+	}
+
+	return $output;
 }
