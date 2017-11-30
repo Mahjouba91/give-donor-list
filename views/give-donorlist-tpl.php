4d70cc1028a38cf9fd43d81c54d750fc7fab3e20
Florian TIAR <contact@tiar-florian.fr>
2017-11-30 21:20:09 +0100

    Fix markup, and date time zone fatal error

--- views/give-donorlist-tpl.php
+++ views/give-donorlist-tpl.php
@@ -1,29 +1,35 @@
 <?php
-if ( isset( $dons_query ) && $dons_query->have_posts() ) : 
+if ( isset( $dons_query ) && $dons_query->have_posts() ) :
 	$date_format = apply_filters( 'GDL/Shortcode/dateformat', 'd F Y, H:i' ); ?>
 
-	<ul class="donor-list">
+    <ul class="donor-list">
 		<?php while ( $dons_query->have_posts() ) : $dons_query->the_post();
-			$meta = get_post_meta( get_the_ID() );
+			$meta        = get_post_meta( get_the_ID() );
 			$paymentmeta = $meta['_give_payment_meta'];
-			$getmeta = maybe_unserialize( $paymentmeta[0] );
+			$getmeta     = maybe_unserialize( $paymentmeta[0] );
 
 			$firstname = esc_html( $getmeta['user_info']['first_name'] );
 			$lastname  = esc_html( $getmeta['user_info']['last_name'] );
-			$name 	   = $firstname . ' ' . $lastname;
-			$total     = $meta['_give_payment_total'][0]; 
+			$name      = $firstname . ' ' . $lastname;
+			$total     = $meta['_give_payment_total'][0];
 
-			$date = new DateTime( reset( $meta['_give_completed_date'] ), get_option('timezone_string') );
+			$date = new DateTime( reset( $meta['_give_completed_date'] ), new DateTimeZone( get_option( 'timezone_string' ) ) );
 			$date = date_i18n( $date_format, $date->getTimestamp() );
 			?>
 
-			<li>
-				<strong><?php echo give_currency_filter( give_format_amount( $total ) ); ?></strong>
-                <strong><?php echo $name; ?></strong>
-                <strong><?php echo esc_html( $date ); ?></strong>
-			</li>
+            <li>
+                <div>
+                    <strong><?php echo give_currency_filter( give_format_amount( $total ) ); ?></strong>
+                </div>
+                <div>
+                    <span><?php echo $name; ?></span>
+                </div>
+                <div>
+                    <span><?php echo esc_html( $date ); ?></span>
+                </div>
+            </li>
 		<?php endwhile;
 		wp_reset_postdata(); ?>
-	</ul>
+    </ul>
 <?php endif;
 wp_reset_query();
